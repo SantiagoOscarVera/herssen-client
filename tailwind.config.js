736d620a419a6645d44e2219070a71903f41a50d
SantiagoOscarVera <santiagovera280@gmail.com>
2023-05-28 10:28:11 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        /* 'qaliBlue': '#132B5B',
+        'qaliGreen': '#0FFFA9',
+        'qaliLightGrey': '#D4D6E4',
+        'qaliGrey': '#969696',
+        'qaliHover': '#041655E5' */
+      }
+    },
+  },
+  plugins: [],
+  safelist: [
+    /* 'qaliBlue',
+    'qaliGreen',
+    'qaliLightGrey',
+    'qaliGrey', */
+  ]
+}
+
+
