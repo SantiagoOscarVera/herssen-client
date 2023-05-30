be1610aad7fbd3a919baaf4e93ec625544a6b7b8
SeroTomas <serocontacto@gmail.com>
2023-05-30 13:59:31 -0300

    estructura navDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home"
 import PricingArtist from "./components/Artist/Pricing/PricingArtist";
+import ArtistDashboard from "./components/Artist/Dashboard/ArtistDashboard";
+import ArtistDashNavBar from "./components/Artist/Dashboard/ArtistDashNavBar";
 
 function App() {
   return (
-    <><Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/artist" element={<PricingArtist/>}/>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/artist" element={<PricingArtist />} />
+        <Route exact path="/artist/dashboard" element={<LayoutArtistDashboard/>} />
       </Routes>
     </>
+  )
+}
+
+function LayoutArtistDashboard() {
+  return (
+    <Routes>
+      <Route path="/" element={<ArtistDashNavBar/>}/>
+      <Route exact path="/" element={<ArtistDashboard/>}/>
+    </Routes>
   )
 }
 
---
+++ src/components/Artist/Dashboard/ArtistDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArtistDashboard() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ArtistDashboard
