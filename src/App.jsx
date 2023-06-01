47598b5447a0e906a4ebeb7f385fb413c1741f98
SantiagoOscarVera <santiagovera280@gmail.com>
2023-05-31 23:35:04 -0300

    form artista diseño y validaciones, le faltan cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home"
 import PricingArtist from "./components/Artist/Pricing/PricingArtist";
+import FormArtist from "./components/Artist/FormArtist/FormArtist";
 
 function App() {
   return (
     <><Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/artist" element={<PricingArtist/>}/>
+        <Route exact path="/artist/form" element={<FormArtist/>}/>
       </Routes>
     </>
   )
 }
+
 
 export default App
