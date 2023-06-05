a83a7671730df09290a65606c902f2d5d1bfd61a
SeroTomas <serocontacto@gmail.com>
2023-06-05 16:16:57 -0300

    usuarios de prueba

---
+++ src/components/service/users.js
@@ -0,0 +1,17 @@
+export const getArtistUser = () => {
+    return {
+        id:12345,
+        email:"tomy.@gmail.com",
+        nick:"TomyARTIST",
+        type: "artist",
+    }
+}
+
+export const getBuyerUser = () => {
+    return {
+        id:54321,
+        email:"tomyBuyer.@gmail.com",
+        nick:"TomyBUYER",
+        type: "buyer",
+    }
+}
