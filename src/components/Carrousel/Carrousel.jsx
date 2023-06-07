10dee665cbfb4099f47d1f7c7f5d4f7241d25627
SeroTomas <serocontacto@gmail.com>
2023-06-07 16:07:53 -0300

    fixing carrousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -3,9 +3,8 @@ import { Button } from 'primereact/button';
 import { Carousel } from 'primereact/carousel';
 import { Tag } from 'primereact/tag';
 
-const Carrousel = ({product}) => {
+const Carrousel = ({ product }) => {
 
-    console.log(product);
     const responsiveOptions = [
         {
             breakpoint: '1199px',
@@ -42,17 +41,19 @@ const Carrousel = ({product}) => {
 
     const productTemplate = (product) => {
         return (
-            <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
-                <div className="mb-3">
-                    <img src={product.image} alt={product.name} className="w-6 shadow-2" />
-                </div>
-                <div>
-                    <h4 className="mb-1">{product.name}</h4>
-                    <h6 className="mt-0 mb-3">${product.price}</h6>
-                    <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
-                    <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
-                        <Button icon="pi pi-search" className="p-button p-button-rounded" />
-                        <Button icon="pi pi-star-fill" className="p-button-success p-button-rounded" />
+            <div className='bg-black flex justify-center'>
+                <div className='flex flex-col'>
+                    <div>
+                        <img src={"https://lh3.googleusercontent.com/lSPcRd7JuGf4uJEGIruV_rothudtLMqoWSl3OEFeRe30Ag3429987e4Bcib3ZjfvvLgaVOnz_1zpTXdWWcmyFAgFzd8iQcHVS-VMhqkHhA"} alt={product.name} className="rounded-t-2xl h-40" />
+                    </div>
+                    <div className='text-black bg-white'>
+                        <h4 className="">{product.name}</h4>
+                        <h6 className="">${product.price}</h6>
+                        <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
+                        <div className="">
+                            <Button icon="pi pi-search" className="" />
+                            <Button icon="pi pi-star-fill" className="" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -61,8 +62,7 @@ const Carrousel = ({product}) => {
 
     return (
         <div className="card">
-            <Carousel value={product } numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} className="custom-carousel" circular
-                autoplayInterval={3000} itemTemplate={productTemplate} />
+            <Carousel value={product} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={productTemplate} />
         </div>
     )
 }
--- src/components/Marketplace/CarrouselContainer/CarrouselContainer.jsx
+++ src/components/Marketplace/CarrouselContainer/CarrouselContainer.jsx
@@ -3,7 +3,7 @@ import Carrousel from '../../Carrousel/Carrousel'
 import { ProductService } from '../../service/ProductService'
 function CarrouselContainer() {
 
-  const [product, setProduct] = useState(ProductService.getProductsData().slice(0,10))
+  const [product, setProduct] = useState(ProductService.getProductsData().slice(0,15))
   console.log(product);
   return (
     <Carrousel product={product} />
