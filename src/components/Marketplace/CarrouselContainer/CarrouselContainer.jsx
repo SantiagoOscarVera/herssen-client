a2bc2ce39bf1fa69c9db5c3d53137e8830f5a278
SeroTomas <serocontacto@gmail.com>
2023-06-07 13:23:04 -0300

    nuevo slice

---
+++ src/components/Marketplace/CarrouselContainer/CarrouselContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Carrousel from '../../Carrousel/Carrousel'
+
+function CarrouselContainer() {
+  return (
+    <div>CarrouselContainer</div>
+  )
+}
+
+export default CarrouselContainer
