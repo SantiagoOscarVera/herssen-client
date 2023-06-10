42afc117ffb6cfb41c21eeb595abe22e259fb4ab
SeroTomas <serocontacto@gmail.com>
2023-06-10 17:19:44 -0300

    working on detail

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import Layout from '../Layouts/BuyerDashLayout';
+import { useSelector } from 'react-redux';
 
 function Detail() {
     const { id } = useParams();
-
-    
+    const {detail} = useSelector(state => state.detail);
+    console.log(detail);
     return (
         <Layout>
             <div>Detail {id}</div>
--- src/redux/store/detail/detailSlice.jsx
+++ src/redux/store/detail/detailSlice.jsx
@@ -1,16 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { ProductService } from '../../../components/service/ProductService';
 
 
 export const detailSlice = createSlice({
   name: 'detail',
-  initialState: {},
+  initialState: {
+    detail: {}
+  },
   reducers: {
-    
+    getItem: (state, { payload }) => {
+      state.detail = payload;
+    }
   },
 });
 
-export const {} = detailSlice.actions;
+export const { getItem} = detailSlice.actions;
 
-
+export const getDetailItem = (id) => (dispatch) => {
+  const products = ProductService.getProductsData();
+  const itemById = products.filter(element => element.id === id);
+  dispatch(getItem(itemById));
+}
 
 export default detailSlice.reducer;
