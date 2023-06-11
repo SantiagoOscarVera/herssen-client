06b95d1049c36b93d8e573dc5a6be36034d95795
SeroTomas <serocontacto@gmail.com>
2023-06-11 13:32:28 -0300

    cards retocadas

--- src/components/Artist/Dashboard/MyCollection/CollectionContainer.jsx
+++ src/components/Artist/Dashboard/MyCollection/CollectionContainer.jsx
@@ -1,18 +1,23 @@
 
-import React from 'react'
-import CardCollection from './CardCollection';
+import React, { useState } from 'react'
+import Card from '../../../Card/Card';
+import { ProductService } from '../../../service/ProductService';
 
 function CollectionContainer() {
 
     // en este componente se hara la logica para traer las diferentes piezas que se hayan creado 
-
-    const array = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7];
+    const [product, setProduct] = useState(ProductService.getProductsData().slice(0, 15))
 
     return (
         <div className='flex flex-wrap w-4/6 gap-4 justify-center'>
             {
-                array.map(element => <CardCollection
-                    
+                product.map(element => <Card
+                id={element.id}
+                    name={element.name}
+                    image="https://picsum.photos/200/200"
+                    artist={element.artist}
+                    price={element.price}
+
                 />)
             }
         </div>
